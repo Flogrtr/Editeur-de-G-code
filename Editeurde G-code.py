f2777d772f1861f505a2e6419500a8f26d9b4792
Flogrtr <78073150+Flogrtr@users.noreply.github.com>
2023-05-16 16:05:04 -0400

    debut
    debut

---
+++ Editeurde G-code.py
@@ -0,0 +1,6 @@
+nombre_section_temperature=int(input("Choisir le nombre de morceaux de d'evolution de temperature linéaire"))
+nombre_section_vitesse=int(input("Choisir le nombre de morceaux de d'evolution de vitesse linéaire"))
+choix_temperature=[]
+for i in range(0,nombre_section_temperature+1)
+
+choix_
